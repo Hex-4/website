6020a3339f5c1841bfcdd1ae4ba3219e7d81676e
Hex-4 <121138331+Hex-4@users.noreply.github.com>
2024-10-04 20:48:22 -0600

    basic stuff

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+			  sans: ['"JetBrains Mono"', ...defaultTheme.fontFamily.sans]
+			}
+		}
+	},
+	plugins: [require("@catppuccin/tailwindcss")],
+}
