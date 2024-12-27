c36f90625108eb15df73c049f72f5f2dc9863ad6
Hex-4 <121138331+Hex-4@users.noreply.github.com>
2024-12-26 17:13:12 -0700

    finish projects, start on blog

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,5 +10,5 @@ export default {
 			}
 		}
 	},
-	plugins: [require("@catppuccin/tailwindcss"), require('tailwindcss-motion')],
+	plugins: [require("@catppuccin/tailwindcss"), require('tailwindcss-motion'), require('@tailwindcss/typography'),],
 }
